70ce658832347a8b06a87658a0f49523cc53ef93
Deepjyoti Borah <deepborah86@gmail.com>
2021-12-02 22:15:15 +0530

    Merge two array

---
+++ practice/src/com/deep/leetcode/MergeSortedArrays.java
@@ -0,0 +1,36 @@
+package com.deep.leetcode;
+
+import com.deep.algoritms.sorting.mergeSort.MergeSort;
+
+import java.util.Arrays;
+
+public class MergeSortedArrays {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] arr = new int[m+n];
+        int k = 0, i = 0, j = 0;
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j])
+                arr[k++] = nums1[i++];
+            else
+                arr[k++] = nums2[j++];
+        }
+
+        while (i < m) {
+            arr[k++] = nums1[i++];
+        }
+
+        while (j < n) {
+            arr[k++] = nums2[j++];
+        }
+        for (int index = 0; index < arr.length; index++)
+            nums1[index] = arr[index];
+    }
+
+    public static void main(String[] args) {
+        MergeSortedArrays mergeSortedArrays = new MergeSortedArrays();
+        int[] l1 = new int[]{1,2,3,0,0,0};
+        int[] l2 = new int[]{2,5,6};
+        mergeSortedArrays.merge(l1, 3, l2, 3);
+        System.out.println(l1);
+    }
+}
